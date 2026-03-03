a96c8855a36ac9f67c5bdfd9af2c196ad82358fc
Firas Jolha <36399502+firas-jolha@users.noreply.github.com>
2026-03-03 16:13:41 +0300

    Update reducer.py

--- app/mapreduce/inverted_index/reducer.py
+++ app/mapreduce/inverted_index/reducer.py
@@ -1,16 +1,38 @@
 import sys
 
-key   = None
-total = 0
-index = {}
-for line in sys.stdin:
-    word, loc  = line.split('\t', 1)
-    word = word.strip()
-    loc = loc.strip()
-    if word in index:
-        index[word].add(loc)
-    else:
-        index[word] = set([loc])
+current_word = None
+current_docs = []  # Store document IDs for current word
 
-for word, docs in index.items():
-    print('{}\t{}'.format(word, docs))
+for line in sys.stdin:
+    line = line.strip()
+    
+    if not line:
+        continue
+    
+    # Parse input
+    try:
+        word, doc_id = line.split('\t', 1)
+    except ValueError:
+        continue
+    
+    # New word encountered
+    if current_word != word:
+        # Output previous word's document list
+        if current_word:
+            # Remove duplicates by converting to set, then sort
+            unique_docs = sorted(set(current_docs))
+            docs_str = '[' + ','.join(unique_docs) + ']'
+            print(f"{current_word}\t{docs_str}")
+        
+        # Reset for new word
+        current_word = word
+        current_docs = [doc_id]
+    else:
+        # Same word, add document ID
+        current_docs.append(doc_id)
+
+# Output last word
+if current_word:
+    unique_docs = sorted(set(current_docs))
+    docs_str = '[' + ','.join(unique_docs) + ']'
+    print(f"{current_word}\t{docs_str}")
